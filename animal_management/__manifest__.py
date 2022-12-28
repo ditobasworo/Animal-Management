f9cf3fa51a0a0da5352249b7742659b5f8474006
dito basworo <muditotejobasworo@gmail.com>
2022-12-28 21:19:31 +0700

    membuat module baru dan model appointment

--- animal_management/__manifest__.py
+++ animal_management/__manifest__.py
@@ -1,35 +1,22 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "animal_management",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+    'name': "Animal Management",
+    'summary': "Modul ini digunakan untuk management animal",
     'description': """
-        Long description of module's purpose
+        modul ini akan dikembangkan lagi
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'author': "Dito",
+    'website': "http://www.dito.odoo.com",
     'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'version': '14.0.1',
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        
+
     ],
-    # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
--- animal_management/models/models.py
+++ animal_management/models/models.py
@@ -1,18 +1,5 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
+class AmAppointment(models.Model):
+    _name = 'am.appointment'
 
-
-# class animal_management(models.Model):
-#     _name = 'animal_management.animal_management'
-#     _description = 'animal_management.animal_management'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
